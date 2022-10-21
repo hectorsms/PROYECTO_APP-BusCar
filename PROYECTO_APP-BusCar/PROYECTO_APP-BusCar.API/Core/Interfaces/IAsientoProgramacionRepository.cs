f0fc0f683504319a3ddbbd1471a8676aa454de0c
tebaniel <69996882+tebaniel@users.noreply.github.com>
2022-10-20 22:16:37 -0500

    up

---
+++ PROYECTO_APP-BusCar/PROYECTO_APP-BusCar.API/Core/Interfaces/IAsientoProgramacionRepository.cs
@@ -0,0 +1,13 @@
+﻿using PROYECTO_APP_BusCar.API.Core.Entities;
+
+namespace PROYECTO_APP_BusCar.API.Infrastructure.Repositories
+{
+    public interface IAsientoProgramacionRepository
+    {
+        Task<bool> Delete(int id);
+        Task<IEnumerable<AsientoProgramacion>> GetAsientoProgramacion();
+        Task<AsientoProgramacion> GetAsientoProgramacion(int id);
+        Task<bool> Insert(Asiento asiento);
+        Task<bool> Update(Asiento asiento);
+    }
+}
---
+++ PROYECTO_APP-BusCar/PROYECTO_APP-BusCar.API/Core/Interfaces/ICargoRepository.cs
@@ -0,0 +1,13 @@
+﻿using PROYECTO_APP_BusCar.API.Core.Entities;
+
+namespace PROYECTO_APP_BusCar.API.Infrastructure.Repositories
+{
+    public interface ICargoRepository
+    {
+        Task<bool> Delete(int id);
+        Task<Cargo> GetCargo(int id);
+        Task<IEnumerable<Cargo>> GetCargos();
+        Task<bool> Insert(Cargo cargo);
+        Task<bool> Update(Cargo cargo);
+    }
+}
