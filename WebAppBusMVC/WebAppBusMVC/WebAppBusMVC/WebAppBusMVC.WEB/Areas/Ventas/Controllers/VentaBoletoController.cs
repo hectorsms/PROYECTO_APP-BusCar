fcb28e9d9afe0f979691d09e464c4cd43ad2c724
hectorsms <hmendoza8999@hotmail.com>
2022-11-13 12:01:02 -0500

    Fronentd

---
+++ WebAppBusMVC/WebAppBusMVC/WebAppBusMVC/WebAppBusMVC.WEB/Areas/Ventas/Controllers/VentaBoletoController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppBusMVC.WEB.Services;
+
+namespace WebAppBusMVC.WEB.Areas.Ventas.Controllers
+{
+    [Area("Ventas")]
+    public class VentaBoletoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult VentaBoleto()
+        //{
+
+        //    return View();
+        //}
+
+        public async Task<IActionResult> VentaBoleto()
+        {
+            var destinos = await VentaBoletoService.GetDestinos();
+            var origenes = await VentaBoletoService.GetOrigenes();
+            ViewBag.ListaDestinos = destinos;
+            ViewBag.ListaOrigenes = origenes;
+            return View();
+
+        }
+
+        public IActionResult VentaBoletoProgramacion()
+        {
+            return View();
+        }
+
+        public IActionResult VentaBoletoProgramacionList()
+        {
+            return PartialView();
+        }
+
+        public IActionResult VentaBoletoAsiento()
+        {
+            return View();
+        }
+
+        public IActionResult VentaBoletoAsientoList()
+        {
+            return PartialView();
+        }
+        public IActionResult VentaBoletoDatosPersonales()
+        {
+                return View();
+        }
+
+        public IActionResult VentaBoletoPago()
+        {
+            return View();
+        }
+
+
+    }
+    }
---
+++ WebAppBusMVC/WebAppBusMVC/WebAppBusMVC/WebAppBusMVC.WEB/Services/VentaBoletoService.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using WebAppBusMVC.WEB.Models;
+
+namespace WebAppBusMVC.WEB.Services
+{
+    public class VentaBoletoService
+    {
+
+        public static async Task<IEnumerable<DestinoViewModel>> GetDestinos()
+        {
+            var url = "http://localhost:5276/api/Destino" ;
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.GetAsync(url);
+
+            var apiResponse = await response.Content.ReadAsStringAsync();
+            var destinoResponse = JsonConvert.DeserializeObject<IEnumerable<DestinoViewModel>>(apiResponse);
+
+            return destinoResponse;
+
+        }
+
+        public static async Task<IEnumerable<OrigenViewModel>> GetOrigenes()
+        {
+            var url = "http://localhost:5276/api/Origen";
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.GetAsync(url);
+
+            var apiResponse = await response.Content.ReadAsStringAsync();
+            var origenResponse = JsonConvert.DeserializeObject<IEnumerable<OrigenViewModel>>(apiResponse);
+
+            return origenResponse;
+
+        }
+    }
+}
